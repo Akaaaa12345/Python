9b97cb46e3fe15aec66a1e89790495f3559a9c9a
2022864047_채수형 <cotngud0505@naver.com>
2026-03-12 18:04:48 +0900

    Add files via upload

--- 0306/hello.py
+++ 0306/hello.py
@@ -1,2 +1,25 @@
 print("Hello python world!")
 print("Welcome to python world")
+
+
+
+print("환영합니다.")
+print("파이썬의 세계로 오신 것을 환영합니다.")
+print("파이썬은 강력합니다.")
+
+
+
+print("+===============+")
+print(":     홍길동     :")
+print("+===============+")
+
+
+
+print(1+2+3)
+
+
+print("1+2+3을 계산하면 6입니다.")
+
+
+
+
